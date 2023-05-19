ccc39a7696b9bda59d6808ed89e84ccda9c6ad71
C. Felix Lee <felixlee.music@gmail.com>
2023-05-19 11:10:18 -0400

    fetching data with grapQL

---
+++ Exercise Files/Ch06/06_04/start/react-app/src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const query = `
+query {
+  allLifts {
+    name
+    elevationGain
+    status
+  }
+}`;
+
+const opts = {
+  method: "POST",
+  headers: { "Content-type": "application/json" },
+  body: JSON.stringify({ query }),
+};
+function Lift({ name, elevationGain, status }) {
+  return (
+    <>
+      <h1>{name}</h1>
+      <p>
+        {elevationGain} {status}
+      </p>
+    </>
+  );
+}
+
+function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    //fetch() is built into the browser for making HTTP request from whatever source
+    fetch(`http://snowtooth.moonhighway.com/`, opts)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (!data) return null;
+  console.log(data, "DATA!!!");
+  return (
+    <div>
+      {data.data.allLifts.map((lift) => (
+        <Lift name={lift.name} elevationGain={lift.elevationGain} status={lift.status} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
