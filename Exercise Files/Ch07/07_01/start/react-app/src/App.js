91578264a1ba31829f016ff6f0215c081fa79496
C. Felix Lee <felixlee.music@gmail.com>
2023-05-21 00:12:51 -0400

    setting up react-router

---
+++ Exercise Files/Ch07/07_01/start/react-app/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  );
+}
+function App() {
+  return <h1>App</h1>;
+}
+
+export default App;
